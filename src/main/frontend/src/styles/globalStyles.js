5c133e150646bc8d5fe88e94b7d315e7a027e292
wanna175 <jh5045@naver.com>
2024-09-25 16:32:04 +0900

    🐛 fix     : global css
    css 적용 안되는 경우 그 속성에 !important를 붙이면 적용이 된다.

--- src/main/frontend/src/styles/globalStyles.js
+++ src/main/frontend/src/styles/globalStyles.js
@@ -7,17 +7,18 @@ export const globalStyles = css`
     font-size: 20px;
     font-family: "myFont";
   }
-    /* 기본 토글 버튼 배경색 변경 */
-.ant-layout-sider-trigger {
-    background-color: #ffffff;
-    color: black;
+  /* 기본 토글 버튼 배경색 변경 */
+  .ant-layout-sider-trigger {
+    background-color: #ffffff !important;
+    color: black !important;
   }
-  
+
   /* 마우스 호버 시 배경색 변경 */
   .ant-layout-sider-trigger:hover {
-    background-color: #40a9ff; /* 더 밝은 파란색으로 변경 */
+    background-color: gray !important;
+    color: white !important; /* 더 밝은 파란색으로 변경 */
   }
-  
+
   @font-face {
     //전체 폰트설정은 여기서 해주자고
     font-family: "myFont";
