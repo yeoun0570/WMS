66f190178a4585f90ecf63a9c1fb9c1755c149e6
wanna175 <jh5045@naver.com>
2024-09-29 03:08:36 +0900

    ✨ feat    :  프론트앤드 로그인 구현완료
    내일 회원가입해보고 토큰 발급까지 해볼 예정

--- src/main/frontend/src/component/login/login.container.js
+++ src/main/frontend/src/component/login/login.container.js
@@ -2,8 +2,13 @@ import LoginUI from "./login.presenter";
 import { useForm } from "react-hook-form";
 import { useRouter } from "next/router";
 import { errorModal } from "../../lib/util";
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { useAPI } from "../../axios/useAPI";
+import { TokenContext } from "../../axios/TokenContext";
 export default function Login() {
+  const { post } = useAPI();
+  const { dispatch } = useContext(TokenContext);
+
   const [isModalOpen, setIsModalOpen] = useState(false);
   const router = useRouter();
   const {
@@ -18,11 +23,43 @@ export default function Login() {
     },
     mode: "onChange",
   });
+
+  const login = async () => {
+    try {
+      const loginData = {
+        userId: employeeNumber,
+        password,
+      };
+      const response = await post("/auth/login", loginData);
+
+      // 받은 토큰을 전역 상태와 세션 스토리지에 저장
+      dispatch({
+        type: "SET_TOKENS",
+        payload: {
+          accessToken: response.ACCESS_HEADER_STRING,
+          refreshToken: response.REFRESH_HEADER_STRING,
+        },
+      });
+
+      console.log(response);
+      router.push(`/wms`);
+    } catch (error) {
+      console.error(
+        "login error : ",
+        error.response ? error.response.data.error : error.message
+      );
+      errorModal(
+        "fail",
+        error.response ? error.response.data.error : error.message
+      );
+    }
+  };
+
   const onSubmitBoard = async (data) => {
     try {
       console.log(data);
+      login();
       //rest든 graphql이든 우리 백앤드로 data로 날리는 구간(await)
-      router.push(`/wms`);
     } catch (error) {
       if (error instanceof Error) errorModal("fail", error.message);
     }
