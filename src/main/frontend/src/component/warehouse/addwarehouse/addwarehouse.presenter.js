8cc69a2d9efc0cc53da4a86e365e521541943690
KimJeongu <kjw101147@gmail.com>
2024-10-02 01:01:54 +0900

    💄 design : 창고등록 페이지 추가

--- src/main/frontend/src/component/warehouse/addwarehouse/addwarehouse.presenter.js
+++ src/main/frontend/src/component/warehouse/addwarehouse/addwarehouse.presenter.js
@@ -1,11 +1,56 @@
 import * as S from "./addwarehouse.styles";
+import AddressInput from "./AddressInput";
+import { Button, Modal } from "antd";
+import { useState } from "react";
+
 
 export default function WarehouseUI() {
+
+  const [zonecode, setZonecode] = useState('');
+  const [address, setAddress] = useState('');
+  const [detailedAddress, setDetailedAddress] = useState('');
+
   return (
     <S.Wrapper>
-      창고등록페이지 입니다.
-      <input type="text"></input>
-      <button></button>
+      <h1 style={{fontSize: "24px", marginBottom: "10px"}}>창고 등록</h1>
+      <div style={{width: "100%", height: "1px", border: "solid 1px #f0f0f0", marginBottom: "40px"}}/>
+      <S.Input>
+        <div>담당자</div>
+        <input></input>
+      </S.Input>
+      <div style={{ display: "flex" }}>
+        <S.Input>
+          <div>창고 ID</div>
+          <input></input>
+        </S.Input>
+        <S.Input>
+          <div>창고 이름</div>
+          <input></input>
+        </S.Input>
+      </div>
+      <S.Input>
+        <div>우편 번호</div>
+        <div style={{ display: "flex" }}>
+          <input value={zonecode} readOnly style={{ width: "0px" }} />
+          <AddressInput addressAction={{ setZonecode, setAddress }} />
+        </div>
+      </S.Input>
+      <S.Input>
+        <div>주소</div>
+        <input value={address} readOnly />
+      </S.Input>
+      <S.Input>
+        <div>상세주소</div>
+        <input value={detailedAddress} />
+      </S.Input>
+      <S.Input>
+        <div>면적</div>
+        <input></input>
+      </S.Input>
+      <div style={{width:"100%", marginTop: "30px", display: "flex", justifyContent: "end"}}>
+        <S.MyButton style={{marginRight: "3px"}}>등록</S.MyButton>
+        <S.MyButton style={{marginRight: "10px"}}>취소</S.MyButton>
+      </div>
     </S.Wrapper>
   );
 }
--- src/main/frontend/src/component/warehouse/addwarehouse/addwarehouse.styles.js
+++ src/main/frontend/src/component/warehouse/addwarehouse/addwarehouse.styles.js
@@ -1,6 +1,39 @@
 import styled from "@emotion/styled";
+import theme from "../../../styles/theme";
+import { Button, Modal } from "antd";
 
-export const Wrapper = styled.div`
-  height: 500px;
+export const Wrapper = styled.form`
+  margin: auto;
   width: 700px;
+`;
+
+export const Input = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 10px;
+  margin-right: 10px;
+  margin-bottom: 25px;
+  flex-grow: 1;
+  
+  div {
+    font-weight: normal;
+    font-size: 12px;
+    flex-grow: 1;
+    
+  }
+
+  input {
+    border : solid 1px ${theme.colors.glbGrey};
+    border-radius: 6px;
+    font-weight: normal;
+    padding: 6px;
+    flex-grow: 1;
+  }
+`;
+
+export const MyButton = styled(Button)`
+  &: hover {
+    border-color: ${theme.colors.glbOrange} !important;
+    color: ${theme.colors.glbOrange} !important;
+  }
 `;
