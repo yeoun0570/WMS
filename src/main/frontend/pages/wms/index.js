33c5928a7067c7c857633d0f034e19f396bf0ea1
wanna175 <jh5045@naver.com>
2024-10-02 03:46:13 +0900

    🐛 fix     : 버그 수정

--- src/main/frontend/pages/wms/index.js
+++ src/main/frontend/pages/wms/index.js
@@ -7,6 +7,7 @@ export default function WMSHome() {
   return (
     <>
       <div>로그인성공후 디폴트 페이지.</div>
+      <div>권한없음 페이지 만들기 귀찮아서 테스트 페이지로 보냄</div>
       <button onClick={onClickButton}>셀프로 알림 보내보기.</button>
     </>
   );
--- src/main/frontend/src/layout/layoutPage.js
+++ src/main/frontend/src/layout/layoutPage.js
@@ -22,6 +22,7 @@ import * as P from "./profile/ProfileStyle";
 import ProfileItem from "./profile/ProfileItem";
 import Notifications from "../sse/sseAPI";
 import { SSEContext } from "../sse/sseAPI";
+import ProfileInfo from "../info/info";
 const { Content } = Layout;
 const LOGIN_PAGE = [
   "/login",
@@ -43,6 +44,7 @@ export default function LayoutPage(props) {
 
   const [noticeOpen, setNoticeOpen] = useState(false);
   const [profileOpen, setProfileOpen] = useState(false);
+  const [noticeFlag, setNoticeFlag] = useState(false);
   const [profileFlag, setProfileFlag] = useState(false);
   const modalRef = useRef();
 
@@ -105,7 +107,7 @@ export default function LayoutPage(props) {
     ]),
     getItem("운송장관리", "sub6", <DesktopOutlined />, [
       getItem("운송장조회", "/wms/waybill"),
-      getItem("운송장수정", "/wms/modifywaybill"),
+      getItem("운송장수정", "/wms/waybill/modifywaybill"),
     ]),
   ];
   /////////////////////////////여기 위에 지저분한거 나중에 다른파일로 뺍시다.리팩토링필요
@@ -115,9 +117,25 @@ export default function LayoutPage(props) {
   const onClickNav = ({ item, key, keyPath, domEvent }) => {
     setPage(pageMap[key][0]);
     setDetail(pageMap[key][1]);
-    router.push(key);
+    console.log(profile.role);
+    if (
+      /*profile.role === "GENERAL_MANAGER" &&*/
+      pageMap[key][0] === "회원관리" ||
+      pageMap[key][0] === "창고관리"
+    ) {
+      if (profile.role !== "GENERAL_MANAGER") {
+        router.push("/wms");
+      } else {
+        router.push(key);
+      }
+    } else {
+      router.push(key);
+    }
   };
 
+  const handleNewNotification = (newNotice) => {
+    setNotices((prevNotices) => [...prevNotices, newNotice]);
+  };
   // 알림 창 띄우기
   const showNotice = () => {
     setNoticeOpen(true);
@@ -128,19 +146,20 @@ export default function LayoutPage(props) {
   const showProfile = async () => {
     setProfileOpen(true);
     setLoading(true);
-    if (!profileFlag) getUserInfo();
+    //if (!profileFlag) getUserInfo();
     setProfileFlag(true);
   };
   //내 프로필 정보 받아오기
   const getUserInfo = async () => {
     try {
       const response = await get("user/info");
-      console.log(response.data);
+      console.log(response.data.userPosition);
       setProfile({
         id: response.data.userId,
         url: response.data.userProfile,
         name: response.data.userName,
         email: response.data.userEmail,
+        role: response.data.userPosition,
       });
     } catch (error) {
       setProfile({
@@ -172,6 +191,7 @@ export default function LayoutPage(props) {
 
   return (
     <>
+      {!isLoginPage && <ProfileInfo getUserInfo={getUserInfo} />}
       <Layout
         style={{
           minHeight: "100vh",
@@ -188,7 +208,12 @@ export default function LayoutPage(props) {
         >
           {!isLoginPage && (
             <>
-              <Notifications setNotices={setNotices} notices={notices} />
+              <Notifications
+                handleNewNotification={handleNewNotification}
+                notices={notices}
+                noticeFlag={noticeFlag}
+                setNoticeFlag={setNoticeFlag}
+              />
               <LayoutHeader
                 router={router}
                 setPage={setPage}
@@ -223,7 +248,8 @@ export default function LayoutPage(props) {
                 {notices.map((el) => {
                   // JSON 문자열을 파싱해서 실제 객체로 변환
 
-                  const parsedData = JSON.parse(el.data);
+                  console.log("ddddd", el);
+                  const parsedData = JSON.parse(el);
 
                   console.log("통지값이 대체 뭔지", parsedData);
 
@@ -242,9 +268,11 @@ export default function LayoutPage(props) {
 
           {/* 프로필 창 */}
           {profileOpen && (
-            <P.ProfileModal ref={modalRef}>
-              <ProfileItem profile={profile} />
-            </P.ProfileModal>
+            <>
+              <P.ProfileModal ref={modalRef}>
+                <ProfileItem profile={profile} />
+              </P.ProfileModal>
+            </>
           )}
 
           <Content
